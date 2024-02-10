3f4ac5e0a952d6a3f4d1c780bac83e1f91bf9b95
GeanVas <gean.vasconez@gmail.com>
2024-02-10 00:32:10 -0500

    final mobile design

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const initialPositions = {
   card1: { translateX: -80, translateY: -100 },
-  card2: { translateX: -80, translateY: 140},
-  card4: { translateX: 80, translateY: 140},
+  card2: { translateX: -80, translateY: 120},
+  card4: { translateX: 80, translateY: 120},
   card3: { translateX: 80, translateY: -100 },
 };
 
@@ -43,4 +43,22 @@ anime({
       }
     });
   },
+});
+
+const dialog = document.querySelector("#response");
+const messages = {
+  card1: "As the King of Wands, I offer you ardent love and a life full of adventure. Are you ready to join me on this journey?",
+  card2: "The Page of Pentacles brings with it opportunities and growth. Are you ready to embark on a journey of discovery and prosperity with me?",
+  card3: "Like the Tower, our love can withstand the tests of time and storms. Are you ready to face the changes with me and find the beauty in rebuilding our destiny together?",
+  card4: "Just like the Two of Wands, together we are stronger, bolder, and more capable of conquering the world of love and beyond. Are you ready to join me in writing our own story full of adventure and love?",
+};
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    const id = card.id;
+    const message = messages[id];
+    dialog.textContent = message;
+    dialog.showModal();
+  });
 });
